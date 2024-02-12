8e376f39fc0e284df16a333d5c66090e975a5510
Gustavo <gustavo.correa2001@outlook.com>
2024-02-12 13:57:34 -0300

    Mudanças no admin, models, urls, index e fazendo o site parecer mais amigável

--- contact/models.py
+++ contact/models.py
@@ -1,17 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
+
+# Depois
+# owner (foreign key)
+
+
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-   
-   
-    name = models.CharField(max_length = 50)
+
+    name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
-        return f'{self.name}'
-    
+        return self.name
 
 
 class Contact(models.Model):
@@ -25,17 +33,15 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
-        on_delete = models.SET_NULL,
-        blank = True, null = True,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
     )
     owner = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        blank = True,
-        null = True
+        blank=True, null=True
     )
-    
-    
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
     
