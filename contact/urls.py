e0101ff54369370a994539dbd15a16f66aa01bee
Gustavo <gustavo.correa2001@outlook.com>
2024-02-14 11:44:02 -0300

    Mudando o layout de contacts, melhorando o sistema de busca, e adicionando páginas.

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,12 @@ app_name = 'contact'
 
 
 urlpatterns = [
-    path('<int:contact_id>',views.contact, name='contact' ), #type:ignore
+    
+    #contact (CRUD)
+    
     path('',views.index, name='index' ), #type:ignore
+    path('search/',views.search, name='search' ), #type:ignore
+    
+    path('contact/<int:contact_id>/',views.contact, name='contact' ), #type:ignore
+    path('contact/create/',views.create, name='create' ), #type:ignore
 ]
