578f2ec273544f25c962219a335db9dcf7eb0007
Gustavo <gustavo.correa2001@outlook.com>
2024-02-15 11:44:37 -0300

    Autenticação de usuários, aumento de segurança no site

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
@@ -15,13 +14,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-
     name = models.CharField(max_length=50)
-
     def __str__(self) -> str:
         return self.name
-
-
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -41,7 +36,6 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
-
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
     
--- contact/urls.py
+++ contact/urls.py
@@ -1,18 +1,16 @@
+# type:ignore
 from django.urls import path
 from contact import views
-
-
 app_name = 'contact'
-
-
-
 urlpatterns = [
-    
-    #contact (CRUD)
-    
-    path('',views.index, name='index' ), #type:ignore
-    path('search/',views.search, name='search' ), #type:ignore
-    
-    path('contact/<int:contact_id>/',views.contact, name='contact' ), #type:ignore
-    path('contact/create/',views.create, name='create' ), #type:ignore
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
 ]
