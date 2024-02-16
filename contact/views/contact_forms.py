ba88ff55f00acc7cf1fc7c4124fd96bfa152601f
Gustavo <gustavo.correa2001@outlook.com>
2024-02-16 12:05:08 -0300

    Partes finais do projeto

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,21 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
-
     if request.method == 'POST':
-        form = ContactForm(request.POST)
         form = ContactForm(request.POST, request.FILES)
-
         context = {
             'form': form,
             'form_action': form_action,
         }
+
         if form.is_valid():
             contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
+
         return render(
             request,
             'contact/create.html',
@@ -30,16 +34,16 @@ def create(request):
         'contact/create.html',
         context
     )
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
         form = ContactForm(request.POST, request.FILES, instance=contact)
-
         context = {
             'form': form,
             'form_action': form_action,
@@ -61,11 +65,14 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     confirmation = request.POST.get('confirmation', 'no')
+
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
